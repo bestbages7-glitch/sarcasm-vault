05bfe0299a2805ec44905de9cc7ca1f405238c24
bestbages7-glitch <bestbages7@gmail.com>
2026-09-21 18:58:15 +0300

    Update get-sarcasm.js

--- netlify/functions/get-sarcasm.js
+++ netlify/functions/get-sarcasm.js
@@ -75,7 +75,7 @@ Respond ONLY with a valid JSON object matching this exact schema:
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        model: "mixtral-8x7b-32768",
+        "model": "llama-3.3-70b-versatile",
         messages: [
           { role: "system", content: systemPrompt },
           { role: "user", content: `Situation: ${query}` }
