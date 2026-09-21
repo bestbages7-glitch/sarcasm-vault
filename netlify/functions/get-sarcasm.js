846f436d70fbf99efea392e0ea51701b4a73f1d6
bestbages7-glitch <bestbages7@gmail.com>
2026-09-21 19:20:42 +0300

    Update get-sarcasm.js

--- netlify/functions/get-sarcasm.js
+++ netlify/functions/get-sarcasm.js
@@ -61,6 +61,7 @@ Available tones: "Dry Wit", "Sharp Irony", "Passive-Aggressive", "Sarcastic".`;
 
   // Ланцюжок моделей на випадок, якщо одна з них вимкнена або обмежена
   const candidateModels = [
+    "openai/gpt-oss-120b",
     "llama-3.3-70b-versatile",
     "llama-3.1-8b-instant",
     "mixtral-8x7b-32768",
